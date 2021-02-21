c3f04c513977852e2ec5a5d98c0663c5fabf4bfe
Elizaveta <69856251+TheCoolestMango@users.noreply.github.com>
2021-02-21 19:41:42 +0300

    Update warmup_createdb.py

--- lab5/warmup_createdb.py
+++ lab5/warmup_createdb.py
@@ -2,7 +2,7 @@
 # psql -d template1
 import psycopg2
 con = psycopg2.connect(database="customers", user="postgres",
-                       password="becausewut101", host="127.0.0.1", port="55818")
+                       password="becausewut101", host="127.0.0.1", port="5432")
 
 print("Database opened successfully")
 cur = con.cursor()
@@ -10,12 +10,13 @@ cur.execute('''CREATE TABLE CUSTOMER
        (ID INT PRIMARY KEY     NOT NULL,
        Name           TEXT    NOT NULL,
        Address            TEXT     NOT NULL,
+       Age INT NOT NULL,
        review        TEXT);''')
 print("Table created successfully")
 
 print("Record inserted successfully")
 cur = con.cursor()
-cur.execute("SELECT ID, Name, Address, review from CUSTOMER")
+cur.execute("SELECT ID, Name, Address, Age, review from CUSTOMER")
 rows = cur.fetchall()
 
 print(rows)
@@ -23,13 +24,14 @@ for row in rows:
     print("ID =", row[0])
     print("Name =", row[1])
     print("Address =", row[2])
-    print("review =", row[3], "\n")
+    print("Age =", row[2])
+    print("review =", row[4], "\n")
 print("Operation done successfully")
-con.close()
+
 
 from faker import Faker
 fake = Faker()
 for i in range(100000):
     print(i)
-    cur.execute("INSERT INTO CUSTOMER (ID,Name,Address,review) VALUES ('"+ str(i)+"','"+fake.name()+"','"+fake.address()+"','"+fake.text()+"')")
+    cur.execute("INSERT INTO CUSTOMER (ID,Name,Address,Age,review) VALUES ('"+ str(i)+"','"+fake.name()+"','"+fake.address()+"','"+str(fake.random_int(min = 18, max = 70))+"','"+fake.text()+"')")
     con.commit()
