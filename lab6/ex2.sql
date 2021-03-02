1ce12c6f9e6acf147d5a7c0830d9533310ceabec
Elizaveta <69856251+TheCoolestMango@users.noreply.github.com>
2021-03-02 10:32:10 +0300

    Create ex2.sql

---
+++ lab6/ex2.sql
@@ -0,0 +1,12 @@
+EXPLAIN ANALYZE 
+SELECT DISTINCT title,rating,category.name FROM film 
+	JOIN film_category ON film_category.film_id = film.film_id
+	JOIN category ON film_category.category_id = category.category_id
+	JOIN inventory ON film.film_id = inventory.film_id 
+	JOIN rental ON inventory.inventory_id = rental.inventory_id
+WHERE (rating = 'R' OR rating = 'PG-13')
+	AND (category.name = 'Horror' OR category.name = 'Sci-Fi')
+	AND (rental.return_date is not null)
+
+-- the most expensive step is the JOIN (as Nested Loop) (cost = 88.14...294.56)
+-- possible solution for it is to create Indexes (B-tree or Hash)
