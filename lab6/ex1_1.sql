d21cee2673af6ac0a00b47c895262dfac435f1a9
Elizaveta <69856251+TheCoolestMango@users.noreply.github.com>
2021-03-02 10:32:29 +0300

    Update ex1_1.sql

--- lab6/ex1_1.sql
+++ lab6/ex1_1.sql
@@ -1,7 +1,8 @@
-SELECT DISTINCT title,rating,category.name FROM film JOIN film_category ON film_category.film_id = film.film_id
-JOIN category ON film_category.category_id = category.category_id
-JOIN inventory ON film.film_id = inventory.film_id 
-JOIN rental ON inventory.inventory_id = rental.inventory_id
+SELECT DISTINCT title,rating,category.name FROM film 
+	JOIN film_category ON film_category.film_id = film.film_id
+	JOIN category ON film_category.category_id = category.category_id
+	JOIN inventory ON film.film_id = inventory.film_id 
+	JOIN rental ON inventory.inventory_id = rental.inventory_id
 WHERE (rating = 'R' OR rating = 'PG-13')
-AND (category.name = 'Horror' OR category.name = 'Sci-Fi')
-AND (rental.return_date is not null)
+	AND (category.name = 'Horror' OR category.name = 'Sci-Fi')
+	AND (rental.return_date is not null)
